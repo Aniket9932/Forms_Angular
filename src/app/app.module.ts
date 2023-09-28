bd3c7eb89e6f16ac47c83ff1bcf57380cae70beb
Aniket Mahato <aniket.mahata.06@gmail.com>
2023-09-28 22:02:31 +0530

    upload button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -15,7 +15,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -36,7 +36,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatGridListModule,
     MatCheckboxModule,
     MatSliderModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent {
-  name = 'Aniket';
-  
+export class RegistrationComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.metaData.controls['batchType'].valueChanges.subscribe((res) => {
+      if (res === 'Option 2') {
+        this.metaData.controls['subType'].setValidators(Validators.required);
+      } else {
+      }
+    });
+  }
+  metaData: FormGroup = new FormGroup({
+    location: new FormControl('', [Validators.required]),
+    batchType: new FormControl('', [Validators.required]),
+    subType: new FormControl(''),
+    formType: new FormControl(''),
+    year: new FormControl(''),
+    julian: new FormControl(''),
+    toteNo: new FormControl(''),
+    cbh: new FormControl(''),
+    kfp: new FormControl(''),
+    note: new FormControl(''),
+    holdBatch: new FormControl(''),
+    brightenImage: new FormControl(''),
+  });
+
+  fileName: string = '';
+  fileType: string = '';
+
+  scan() {
+    console.log('Hello!');
+  }
+
+  fileUpload(event: any) {
+    event.preventDefault();
+    this.fileName = event.target.files[0].name;
+    this.fileType = event.target.files[0].type.split('/')[1];
+
+    if (this.fileType !== 'pdf') {
+      this.fileName = 'File type not supported';
+      this.fileType = '';
+      return;
+    }
+
+    console.log(event.target.files[0]);
+    console.log(this.fileType);
+    console.log(this.fileName);
+  }
 }
